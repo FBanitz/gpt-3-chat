3e680b6862b720061c6843448e42fe5094a172a9
François BANITZ <francois.banitz@gmail.com>
2022-12-30 18:18:03 +0100

    Create colors.dart

---
+++ lib/styles/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFCECCCC);
+  static const Color secondaryColor = Color(0xFF29339B);
+  static const Color accentColor = Color(0xFFFF3A20);
+  static const Color secondaryAccentColor = Color(0xFFFF7F11);
+  static const Color backgroundColor = Color(0xFF000000);
+  static const Color primaryTextColor = Color(0xFF1E1E1E);
+  static const Color secondaryTextColor = Color(0xFF1E1E1E);
+  static const Color accentTextColor = Color(0xFF1E1E1E);
+}
+
+extension AppColorsExtension on Colors {
+  
+
+}
